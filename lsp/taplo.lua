c6a1552d3aa4c8652c7fab7e604d00318bfaffff
Matt Riley <matthew.riley@spw.com>
2026-04-17 14:45:24 +0100

    chore: more taplo stuff

--- lsp/taplo.lua
+++ lsp/taplo.lua
@@ -1,2 +1,12 @@
 ---@type vim.lsp.Config
-return {}
+return {
+  settings = {
+    taplo = {
+      schema = {
+        associations = {
+          [".*sesh\\.toml$"] = "https://github.com/joshmedeski/sesh/raw/main/sesh.schema.json",
+        },
+      },
+    },
+  },
+}
