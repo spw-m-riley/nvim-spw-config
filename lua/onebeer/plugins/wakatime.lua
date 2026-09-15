977afa5d076e8e039c15a4821878dec40a293162
Matt Riley <matthew.riley@lloydswealth.com>
2026-09-15 10:56:50 +0100

    chore: remove Wakatime plugin

--- lua/onebeer/plugins/wakatime.lua
+++
@@ -1,6 +0,0 @@
----@type onebeer.PluginSpec
-return {
-  "wakatime/vim-wakatime", -- Vim plugin for automatic time tracking and metrics generated from your programming activity.
-  enabled = false,
-  event = "VeryLazy",
-}
